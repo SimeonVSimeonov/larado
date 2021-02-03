a987dbe5629501e7d17ca5d0c1c62ce5a4fe80a4
simeonoff <simeonsimeonoff@icloud.com>
2021-02-03 22:01:53 +0200

    logout route

--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,6 @@ Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 
 Route::group(['middleware' => ['auth:api']], function () {
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
     Route::apiResource('/todos', '\App\Http\Controllers\API\TodoController');
 });
