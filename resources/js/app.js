ba7fac281a2c6aa094654f76b8f20790d5ad7b63
simeonoff <simeonsimeonoff@icloud.com>
2021-02-03 13:04:21 +0200

    add date format filter

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,16 @@
 import Vue from 'vue';
+import moment from 'moment'
 import router from './router';
 import App from "./components/App";
 //require('./bootstrap');
 
 import Vuelidate from 'vuelidate'
 Vue.use(Vuelidate)
+Vue.filter('formatDate', function(value) {
+    if (value) {
+        return moment(String(value)).format('MM/DD/YYYY hh:mm')
+    }
+});
 
 const app = new Vue({
     el: '#app',
