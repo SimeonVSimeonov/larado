14b41bb23ae3d3ca349e7b6f19bcc5dc20db102e
simeonoff <simeonsimeonoff@icloud.com>
2021-02-04 15:16:40 +0200

    get todo by id route

--- resources/js/router.js
+++ resources/js/router.js
@@ -4,6 +4,7 @@ import Home from "./components/Home";
 import login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import TodoList from "./components/Todo/TodoList";
+import Todo from "./components/Todo/Todo";
 
 Vue.use(VueRouter);
 
@@ -12,7 +13,8 @@ export default new VueRouter({
         {path: '/', name: 'home', component: Home},
         {path: '/login', name: 'login', component: login},
         {path: '/register', name: 'register', component: Register},
-        {path: '/my-todos', name: 'todoList', component: TodoList}
+        {path: '/my-todos', name: 'todoList', component: TodoList},
+        {path: '/my-todos/:id', name: 'todo', component: Todo}
     ],
     mode: 'history'
 });
